5b535b3fb8783e477b5ea98f9a15cb4736f79273
milo luo <milo luo>
2017-05-01 22:37:28 +0200

    working first new component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
 
 @Component({
+  selector: 'app-sidebar',
+  template: `
+  <div id="sidebar">
+    Sidebar will go here
+    </div>
+  `
+})
+
+export class SidebarComponent{}
+
+@Component({
   selector: 'app-root',
   template: `
      <div id="container">
-      <div id="sidebar">
-        Sidebar will go here
-        </div>
+     <app-sidebar></app-sidebar>
         <div id="content">
         <div>
           Article list will go here
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { AppComponent } from './app.component';
+import { AppComponent, SidebarComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, SidebarComponent
   ],
   imports: [
     BrowserModule,
