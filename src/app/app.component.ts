3b54f357dc3fc0a980c2ccd122cbd83ed1d5834b
milo luo <milo luo>
2017-05-01 22:31:04 +0200

    Basic working template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,19 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `aaaaaaaaaaa`
+  template: `
+     <div id="container">
+      <div id="sidebar">
+        Sidebar will go here
+        </div>
+        <div id="content">
+        <div>
+          Article list will go here
+          </div>
+          </div>
+     </div>
+  `
 })
 export class AppComponent {
-  title = 'app worksaaaa!';
+  title = 'Ari';
 }
