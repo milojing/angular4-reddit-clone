bdbb92cda7c17901fb4c86b88dddf67a89eecf6e
milo luo <milo luo>
2017-05-02 12:41:30 +0200

    Use new semantic ui css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,9 +20,31 @@ export class SidebarComponent{}
 @Component({
   selector: "app-article",
   template: `
-    <div>
-    <h2>{{article.title}}</h2>
-    <p> {{article.description}}</p>
+    <div class="image">
+    <img src="https://placekitten.com/g/400/300"
+    />
+    </div>
+    <div class="content">
+    <div class="header">
+    {{article.title}}
+    </div>
+    <div class="meta">
+    Voting and votes will go here
+    </div>
+    <div class="meta date">
+    Today
+    </div>
+    <div class="meta description">
+    <p>{{article.description}}</p>
+    </div>
+    <div class="extra">
+    <a href="#"
+    target="_blank"
+    class="ui right floated button primary">
+    Read more
+    <i class="right chevron icon"></i>
+    </a>
+    </div>
     </div>
   `
 })
@@ -33,12 +55,13 @@ export class ArticleComponent {
 @Component({
   selector: 'app-root',
   template: `
-     <div id="container">
+     <div class="ui container">
      <app-sidebar></app-sidebar>
-        <div id="content">
+        <div class="ui divided items">
         <app-article
         *ngFor="let article of articles"
-        [article]="article"></app-article>
+        [article]="article"
+        class="item"></app-article>
           </div>
      </div>
   `
