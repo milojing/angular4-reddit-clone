6baa1e1bee088169e5c99c364f3005aa0428bd36
milo luo <milo luo>
2017-05-02 15:17:37 +0200

    Split component into different directories by using ng generate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import {
-  AppComponent,
-  SidebarComponent,
-  ArticleComponent} from './app.component';
+import { AppComponent } from './app.component';
+
+import {SidebarComponent} from './sidebar/sidebar.component';
+import {ArticleComponent} from './article/article.component';
+
 
 @NgModule({
   declarations: [
