6be233792aeb5340dfd245c4773063beee96e5d6
milo luo <milo luo>
2017-05-02 17:06:23 +0200

    Use interface to convert json to article

--- src/app/article.service.ts
+++ src/app/article.service.ts
@@ -25,8 +25,7 @@ export class ArticleService {
     .then(json => json.articles)
     .then(articles => {
       console.log('json ->', articles);
-      const list = articles.map(article => new Article(article.title, article.description, article.urlToImage));
-      return list;
+      return articles.map(article => Article.fromJSON(article));
     })
     .catch(err => {
       console.log('we got an error', err);
--- src/app/article.ts
+++ src/app/article.ts
@@ -1,5 +1,23 @@
+interface ArticleJSON {
+  title: string;
+  url: string;
+  votes: number;
+  publishedAt: string;
+  description:string;
+  author: string;
+  urlToImage:string;
+}
+
 export class Article {
   public publishedAt: Date;
+  static fromJSON(json:ArticleJSON): Article {
+    let article = Object.create(Article.prototype);
+    return Object.assign(article, json, {
+      votes: json.votes ? json.votes: 0,
+      publishedAt: json.publishedAt ? new Date(json.publishedAt): new Date(),
+      imageUrl: json.urlToImage
+    });
+  }
   constructor(
     public title: string,
     public description: string,
